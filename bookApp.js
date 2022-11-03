088b659b0e65d35e5b4615da757050d2bd9abe75
nishabdShunya <nishantchoubisa92@gmail.com>
2022-11-03 11:08:15 +0530

    different methods of display users on the webpage

--- bookApp.js
+++ bookApp.js
@@ -16,10 +16,10 @@ function onSubmit(event) {
         setTimeout(() => { msg.remove() }, 5000);
     }
     else {
-        // If you want to display on the website
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(`${nameInput.value} | ${emailInput.value} | ${dateInput.value} | ${timeInput.value}`));
-        bookedUsers.appendChild(li);
+        // If you want to display on the website (displays only the currently added users)
+        // const li = document.createElement('li');
+        // li.appendChild(document.createTextNode(`${nameInput.value} | ${emailInput.value} | ${dateInput.value} | ${timeInput.value}`));
+        // bookedUsers.appendChild(li);
 
         // If you want to store in the local storage
         // localStorage.setItem('userAppointmentDetails',`${nameInput.value} | ${emailInput.value} | ${dateInput.value} | ${timeInput.value}`);
@@ -33,8 +33,18 @@ function onSubmit(event) {
         }
         // localStorage.setItem('userAppointmentDetails', JSON.stringify(user));
 
-        // Storing multiple users
+        // Storing multiple users in the local storage (just make the key unique)
         localStorage.setItem(user.email, JSON.stringify(user));
+
+        // Displaying on the website using local storage (displays all that is in the local storage)
+        // Object.keys(localStorage).forEach((key) => {
+        //     const li = document.createElement('li');
+        //     li.appendChild(document.createTextNode(`${JSON.parse(localStorage.getItem(key)).name} | ${JSON.parse(localStorage.getItem(key)).email} | ${JSON.parse(localStorage.getItem(key)).dateOfAppointment} | ${JSON.parse(localStorage.getItem(key)).timeOfAppointment}`));
+        //     bookedUsers.appendChild(li);
+        // })
+
+        // Function method to display users (displays only the currently added users)
+        displayUsers(user);
 
         // Clearing the fields
         nameInput.value = '';
@@ -42,4 +52,9 @@ function onSubmit(event) {
         dateInput.value = '';
         timeInput.value = '';
     }
+}
+// Defining the function displayUsers which we called inside the "event" function
+function displayUsers(userDetails) {
+    const li = `<li> ${userDetails.name} | ${userDetails.email} | ${userDetails.dateOfAppointment} | ${userDetails.timeOfAppointment}`;
+    bookedUsers.innerHTML = bookedUsers.innerHTML + li;
 }
